ef3b8d8e37a4a5c46e977cab192a14069a976ad5
Bruno Barros <brunomiguelbarrospinto@gmail.com>
2022-04-01 20:58:32 +0200

    Add sass dependency

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
-import "./styles/tailwind.css";
+import "./styles/tailwind.scss";
 
 const app = createApp(App);
 
