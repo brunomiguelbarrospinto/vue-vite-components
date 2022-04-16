93a99df82a08604f09fc109e81dadeac5679c880
Bruno Barros <brunomiguelbarrospinto@gmail.com>
2022-04-16 14:49:28 +0200

    Publish Icon Loading Animated component

--- src/lib/main.js
+++ src/lib/main.js
@@ -1,5 +1,11 @@
 // lib/main.js
 import ButtonComponent from "@/components/common/ButtonComponent.vue";
 import IconComponent from "@/components/common/IconComponent.vue";
+import IconLoadingAnimatedComponent from "@/components/common/IconLoadingAnimatedComponent.vue";
 import InteractiveElementComponent from "@/components/common/InteractiveElementComponent.vue";
-export { ButtonComponent, IconComponent, InteractiveElementComponent };
+export {
+  ButtonComponent,
+  IconComponent,
+  IconLoadingAnimatedComponent,
+  InteractiveElementComponent,
+};
