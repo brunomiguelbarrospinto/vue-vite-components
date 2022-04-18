e52f3023028f4e7497d086795ffc6c2fb8d8cbe6
Bruno Barros <brunomiguelbarrospinto@gmail.com>
2022-04-18 18:40:22 +0200

    Update lib/main.js and package version

--- src/lib/main.js
+++ src/lib/main.js
@@ -3,9 +3,11 @@ import ButtonComponent from "@/components/common/ButtonComponent.vue";
 import IconComponent from "@/components/common/IconComponent.vue";
 import IconLoadingAnimatedComponent from "@/components/common/IconLoadingAnimatedComponent.vue";
 import InteractiveElementComponent from "@/components/common/InteractiveElementComponent.vue";
+import TagComponent from "@/components/common/TagComponent.vue";
 export {
   ButtonComponent,
   IconComponent,
   IconLoadingAnimatedComponent,
   InteractiveElementComponent,
+  TagComponent,
 };
